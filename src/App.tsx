ce51c970b9234612ce025d270f8f01c1bf21ed4e
Glauco <glauco.pater@gmail.com>
2022-12-13 14:42:08 +0100

    Setup Project Setup Firebase

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { firebaseConfig } from "./firebase-setup";
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-setup.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmL1nF0b4F5YKq_ODoUrWoZMaAgawgSe0",
+  authDomain: "testapi-fec8e.firebaseapp.com",
+  projectId: "testapi-fec8e",
+  storageBucket: "testapi-fec8e.appspot.com",
+  messagingSenderId: "701024071994",
+  appId: "1:701024071994:web:88970a560eb96a85322003",
+  measurementId: "G-ZJYPHY271E",
+};
+
+// Initialize Firebase
